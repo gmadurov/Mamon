aebee3210068005332611a4956d46cb52c0b4ae5
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-08-22 18:09:14 +0200

    purchase has time

--- app/components/Purchase.js
+++ app/components/Purchase.js
@@ -1,11 +1,10 @@
-import { useContext } from "react";
-import ProductContext from "../context/ProductContext";
-import { StyleSheet, Text, View } from "react-native";
-import { Card } from "@rneui/themed";
+import { useContext } from 'react';
+import ProductContext from '../context/ProductContext';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card } from '@rneui/themed';
 
 const Purchase = ({ purchase }) => {
   const { products } = useContext(ProductContext);
-  // console.log(purchase);
   let total = 0;
   purchase?.orders?.map(
     (order) =>
@@ -13,19 +12,21 @@ const Purchase = ({ purchase }) => {
         products?.find((product) => product.id === order.product).price *
         order.quantity)
   );
+  const created = new Date(purchase?.created);
   return (
     <Card style={styles.container}>
       <Text>
         {/* {holders?.find((holder) => holder.id === purchase?.buyer).name}  */}
-        For a {purchase?.payed ? "payed" : "loged"} total of €
-        {parseFloat(total).toPrecision(total <= 10 ? 3 : total <= 100 ? 4 : 5)}:
+        For a {purchase?.payed ? 'payed' : 'loged'} total of €
+        {parseFloat(total).toPrecision(total <= 10 ? 3 : total <= 100 ? 4 : 5)}{' '}
+        on {created.toDateString()} {created.toLocaleTimeString('nl-NL')}:
       </Text>
       {purchase?.orders?.map((order) => (
-        <View key={"cart product" + order.product}>
+        <View key={'cart product' + order.product}>
           <Text>
-            {order.quantity}{" "}
+            {order.quantity}{' '}
             {products?.find((product) => product.id === order.product).name}
-            {order.quantity > 1 && "s"}
+            {order.quantity > 1 && 's'}
           </Text>
         </View>
       ))}
