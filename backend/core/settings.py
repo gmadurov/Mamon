63228ba640a8dca562c35b7f39c9154ac2cab75a
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-10-05 10:49:37 +0200

    update pipfile and local env

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # set to false before uploading
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("DEBUG") == "True" else False
-LOCAL = not False
+LOCAL = False if os.environ.get("LOCAL") == "False" else True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
