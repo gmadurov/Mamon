ef6bb5cb58c7c6843e7f36fb41bab85d564d6aa0
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-10-07 14:38:20 +0100

    update metis

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from .utils import init_DB
+from .utils.utils import init_DB
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "colorfield",
     "storages",
+    "django_seed",
 ]
 
 MIDDLEWARE = [
@@ -163,14 +164,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_URL = "/static/"
+# start
+MEDIA_URL = "/images/"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
-# print(BASE_DIR)
+MEDIA_ROOT = BASE_DIR / "static/images"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "/staticfiles/"
-
-MEDIA_ROOT = "mediafiles"
-MEDIA_URL = "/mediafiles/"
+### end
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- backend/purchase/admin.py
+++ backend/purchase/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Category, Order, Product, Purchase
+from .models import Category, Order, Product, Purchase, Report, Barcycle
 
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Purchase)
 admin.site.register(Category)
+admin.site.register(Report)
+admin.site.register(Barcycle)
 # admin.site.register(Order) #do not enable
