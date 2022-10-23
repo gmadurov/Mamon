3464b57116a283f8c1abc3aedc08df015dc7f8ea
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-10-23 22:46:20 +0200

    add admin page for staff and admin users

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name="admin"),
     path("api/", include("api.urls")),
     path("", include("users.urls")),
     path(
