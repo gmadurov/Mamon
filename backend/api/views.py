fe81f1e1c1777eead0f7a5a9bfad60dcbc1cd68c
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-10-23 22:16:35 +0200

    Remade login for api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-import jwt
 import requests
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import user_passes_test
@@ -20,7 +19,6 @@ from .serializers import (
     ProductSerializer,
     PurchaseSerializer,
 )
-from .tokens import MyTokenObtainPairSerializer, MyTokenObtainPairView
 
 API_URL = "/api/"
 
@@ -113,10 +111,13 @@ def showProduct(request, pk):
 def showPurchases(request):
     data = request.data
     if request.method == "GET":
-        holder = request.user.holder
-        purschases = holder.purchase_set.all()  # Purchase.objects.all()
-        serializer = PurchaseSerializer(purschases, many=True)
-        return Response(serializer.data)
+        try:
+            holder = request.user.holder
+            purschases = holder.purchase_set.all()  # Purchase.objects.all()
+            serializer = PurchaseSerializer(purschases, many=True)
+            return Response(serializer.data)
+        except:
+            return Response("No purchases")
     if request.method == "POST":
         purchase = Purchase.objects.create(
             buyer=Holder.objects.get(id=data["buyer"]),
@@ -224,58 +225,51 @@ def cateories(request):
         return Response(serializer.data)
 
 
+def loginLedenbase(request):
+    res, ledenbaseUser = safe_json_decode(
+        requests.post(
+            os.environ.get("BACKEND_URL") + "/v2/login/",
+            json={
+                "password": request.data["password"],
+                "username": request.data["username"],
+            },
+        )
+    )
+    if res.status_code != 200:
+        return Response(
+            data=ledenbaseUser,
+            status=res.status_code,
+        )
+
+    user, created = User.objects.get_or_create(
+        username=request.data["username"],
+        first_name=ledenbaseUser["user"]["first_name"],
+        last_name=ledenbaseUser["user"]["last_name"],
+        # user purposely doesnt have a password set here to make sure it
+    )
+    holder, created = Holder.objects.get_or_create(
+        user=user,
+    )
+    holder.ledenbase_id = ledenbaseUser["user"]["id"]
+    holder.image_ledenbase = (
+        os.environ.get("BACKEND_URL") + ledenbaseUser["user"]["photo_url"]
+    )
+    holder.save()
+    return user
+
+
 @api_view(["POST"])
 def LoginAllUsers(request):
-
-    try:
-        if (
-            User.objects.get(username=request.data["username"]).holder.ledenbase_id
-        ) > 1:
-            raise Exception("User is from ledenbase")
+    print(request.data)
+    user1 = User.objects.filter(username=request.data["username"])
+    if user1.exists() and user1.filter(holder__ledenbase_id=0).exists():
+        print("user exists and doesnt have ledenbase id")
         user = authenticate(
             password=request.data["password"],
             username=request.data["username"],
         )
-    except:
-        res, ledenbaseUser = safe_json_decode(
-            requests.post(
-                os.environ.get("BACKEND_URL") + "/v2/login/",
-                json={
-                    "password": request.data["password"],
-                    "username": request.data["username"],
-                },
-            )
-        )
-        if res.status_code != 200:
-            return Response(
-                data=ledenbaseUser,
-                status=res.status_code,
-            )
-        try:
-            # print(ledenbaseUser)
-            holder = Holder.objects.get(ledenbase_id=ledenbaseUser["user"]["id"])
-            user = holder.user
-            holder.image_ledenbase = (
-                os.environ.get("BACKEND_URL") + ledenbaseUser["user"]["photo_url"]
-            )
-            holder.save()
-
-        except:
-            # create user and update holder info as required if user is not in database
-            user = User.objects.create(
-                username=request.data["username"],
-                first_name=ledenbaseUser["user"]["first_name"],
-                last_name=ledenbaseUser["user"]["last_name"],
-                # user purposely doesnt have a password set here to make sure it
-            )
-            holder = Holder.objects.get(
-                user=user,
-            )
-            holder.ledenbase_id = ledenbaseUser["user"]["id"]
-            holder.image_ledenbase = (
-                os.environ.get("BACKEND_URL") + ledenbaseUser["user"]["photo_url"]
-            )
-            holder.save()
+    else:
+        user = loginLedenbase(request)
 
     refresh = RefreshToken.for_user(user)
     response = {"refresh": str(refresh), "access": str(refresh.access_token)}
