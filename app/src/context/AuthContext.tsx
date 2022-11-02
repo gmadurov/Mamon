b8b8f847434bcb71b7e8a5aa2616d10a3d51810d
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-11-02 16:21:11 +0100

    Update api direction

--- app/src/context/AuthContext.tsx
+++ app/src/context/AuthContext.tsx
@@ -14,8 +14,8 @@ export const baseUrl = () => {
   // console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
   if (process.env.NODE_ENV === "development") {
-    // url = "https://mamon.esrtheta.nl";
-    url = "http://10.0.2.2:8000";
+    url = "https://mamon.esrtheta.nl";
+    // url = "http://10.0.2.2:8000";
   } else if (process.env.NODE_ENV === "production") {
     url = "https://mamon.esrtheta.nl";
   } else {
