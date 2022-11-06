432886accbd8d7902fbfff719dbd6d832440d01e
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2022-11-06 22:06:55 +0100

    testing production mode

--- app/src/context/AuthContext.tsx
+++ app/src/context/AuthContext.tsx
@@ -15,9 +15,13 @@ export const baseUrl = () => {
 
   if (process.env.NODE_ENV === "development") {
     url = "https://mamon.esrtheta.nl";
-    url = "http://10.0.2.2:8000";
+    // url = "http://10.0.2.2:8000";
   } else if (process.env.NODE_ENV === "production") {
     url = "https://mamon.esrtheta.nl";
+    showMessage({
+      message: "Production mode",
+      type: "info",
+    });
   } else {
     url = "http://10.0.2.2:8000";
   }
