7eafac31d6fe8afa86cc90e1e71e78ee66f6527a
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2023-02-16 14:39:19 +0100

    redo urls for users

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("", include("users.urls")),
-    path("", include("purchase.urls")),  # this passes all paths under purchases/**/ down to the specified urls.py
+    path("purchases/", include("purchase.urls")),  # this passes all paths under purchases/**/ down to the specified urls.py
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/purchase/urls.py
+++ backend/purchase/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
         "purchases/<str:pk>", views.showPurchase, name="purchase"
     ),  # this would be for a single purchase where pk is a variable representing the purchase id(this is how you can pass parameters to the view function)
     path("overview/products/<str:pk>", views.showProductOverviews, name="overviewProduct"),
-    path("products/", views.showProducts, name="products"),
-    path("product/<int:pk>", views.showProduct, name="product"),
+    path("products/", views.showProducts, name="products overview"),
+    path("product/<int:pk>", views.showProduct, name="product overview"),
     path("barcycles/", views.showBarcycles, name="barcycles"),
     path("barcycle/<str:pk>", views.showBarcycle, name="barcycle"),
     path('upgrade/', views.showUpgrades, name="upgrades"),
