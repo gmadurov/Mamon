8a36b5e3f8351dacb69bbb1f7cb91268195db662
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2023-02-16 14:41:26 +0100

    fix user pages

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -1,3 +1,9 @@
-let page
-let search
-let pageLinks
+function deleteMessage() {
+    const deleteButton = document.getElementById("alert__close");
+    if (deleteButton) {
+      deleteButton.addEventListener("click", function (event) {
+        const messageContainer = event.target.closest(".column");
+        messageContainer.remove();
+      });
+    }
+  }
