4802bfc1e2e488074221059bc38956e9fbe93e42
Gustavo Maduro <dev.gam.vollmer@gmail.com>
2023-04-26 11:26:00 +0200

    tests are functional

--- backend/inventory/tests.py
+++ backend/inventory/tests.py
@@ -1,22 +1,21 @@
 from django.test import TestCase
 from .models import Product, Stock
 from users.tests import BaseCase
+
 # Create your tests here.
 
 
 class InventoryBaseTest(BaseCase):
-    
     def setUp(self):
         super().setUp()
 
-        self.master_stock_units = Stock.objects.create(name="master_stock_1", units='units')
-        self.master_stock_liters = Stock.objects.create(name="master_stock_1", units='liters')
-        self.master_stock_bottles = Stock.objects.create(name="master_stock_1", units='bottles')
-
+        self.master_stock_units = Stock.objects.create(name="master_stock_units", units="units", quantity=100)
+        self.master_stock_liters = Stock.objects.create(name="master_stock_liters", units="liters", quantity=100)
+        self.master_stock_bottles = Stock.objects.create(name="master_stock_bottles", units="bottles", quantity=100)
 
         # creating products
-        self.product_1 = Product.objects.create(name="product_1", price=1.00)
-        self.product_2 = Product.objects.create(name="product_2", price=2.00)
-        self.product_3 = Product.objects.create(name="product_3", price=3.00)
-        self.product_40 = Product.objects.create(name="product_40", price=40.00)
-        self.product_50 = Product.objects.create(name="product_50", price=50.00)
+        self.product_1 = Product.objects.create(name="product_1", price=1.00, master_stock=self.master_stock_units, units=1)
+        self.product_2 = Product.objects.create(name="product_2", price=2.00, master_stock=self.master_stock_liters, units=1)
+        self.product_3 = Product.objects.create(name="product_3", price=3.00, master_stock=self.master_stock_bottles, units=1)
+        self.product_40 = Product.objects.create(name="product_40", price=40.00, master_stock=self.master_stock_liters, units=1)
+        self.product_50 = Product.objects.create(name="product_50", price=50.00, master_stock=self.master_stock_units, units=1)
